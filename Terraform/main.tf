0808937341b8d720ac4252b28e243aa722a610ee
Tim <aquamaingi@gmail.com>
2023-02-01 14:45:05 +0300

    fixed secret Keys

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "eu-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
 
 }
 
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,15 +7,6 @@ variable "type" {
 
 }
 
-variable "secret_key" {
-  default = "Fye2nc+2b2i8HPdfMdizAGrXk5zMa5ttEtL9XalO"
-
-}
-
-variable "access_key" {
-  default = "AKIAY245SVLDEXAOM6O6"
-
-}
 
 variable "vpc_id" {
   default = "vpc-0f6156e3671cf91fb"
