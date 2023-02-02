c841a205941df969f5af7e63763fc6d0ad1f0f37
Tim <aquamaingi@gmail.com>
2023-02-02 14:50:39 +0300

    removed access and secret vars

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,15 +7,6 @@ variable "type" {
 
 }
 
-variable "access_key" {
-  type = string
-  nullable = True
-}
-
-variable "secret_key" {
-  type = string
-  nullable = True
-}
 
 variable "vpc_id" {
   default = "vpc-0f6156e3671cf91fb"
