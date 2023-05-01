8b321a1c181b8affcb109a8091e8049bbeb39966
Merch7x <aquamaingi@gmail.com>
2023-05-01 20:31:51 +0300

    changed directory to Terraform

---
+++ Terraform/Ec2/variables.tf
@@ -0,0 +1,21 @@
+variable "ami" {
+  default = "ami-0f540e9f488cfa27d"
+}
+
+variable "type" {
+  default = "t2.micro"
+
+}
+
+variable "access" {
+  
+}
+
+variable "secret" {
+  
+}
+
+variable "vpc_id" {
+  default = "vpc-0f6156e3671cf91fb"
+
+}
