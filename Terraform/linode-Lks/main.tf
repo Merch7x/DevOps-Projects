beef216a69875361287990d2df093a2452db1725
Merch7x <aquamaingi@gmail.com>
2023-05-02 05:26:00 +0300

    initial lke commit

---
+++ Terraform/linode-Lks/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "1.27.1"
+    }
+  }
+}
+//Use the Linode Provider
+provider "linode" {
+  token = var.token
+}
+
+//Use the linode_lke_cluster resource to create
+//a Kubernetes cluster
+resource "linode_lke_cluster" "foobar" {
+    k8s_version = var.k8s_version
+    label = var.label
+    region = var.region
+    tags = var.tags
+
+    dynamic "pool" {
+        for_each = var.pools
+        content {
+            type  = pool.value["type"]
+            count = pool.value["count"]
+        }
+    }
+}
+
+
