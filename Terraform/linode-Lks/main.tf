14bfffe1400a510a6f64d49d41d700101d1263ef
Merch7x <aquamaingi@gmail.com>
2023-05-05 23:23:16 +0300

    added provider

--- Terraform/linode-Lks/main.tf
+++ Terraform/linode-Lks/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    linode = {
-      source = "linode/linode"
-      version = "1.27.1"
-    }
-  }
-}
-//Use the Linode Provider
-provider "linode" {
-  token = var.token
-}
-
-//Use the linode_lke_cluster resource to create
-//a Kubernetes cluster
 resource "linode_lke_cluster" "istio-test" {
     k8s_version = var.k8s_version
     label = var.label
