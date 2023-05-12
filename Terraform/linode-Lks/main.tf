f1e0e220b97fc306ef734575778298485684b907
Merch7x <aquamaingi@gmail.com>
2023-05-12 20:17:36 +0300

    added second cluster

--- Terraform/linode-Lks/main.tf
+++ Terraform/linode-Lks/main.tf
@@ -13,4 +13,19 @@ resource "linode_lke_cluster" "istio-test" {
     }
 }
 
+# resource "linode_lke_cluster" "second-cluster" {
+#     k8s_version = var.k8s_version
+#     label = var.label
+#     region = var.region
+#     tags = var.tags
+
+#     dynamic "pool" {
+#         for_each = var.pools
+#         content {
+#             type  = pool.value["type"]
+#             count = pool.value["count"]
+#         }
+#     }
+# }
+
 
